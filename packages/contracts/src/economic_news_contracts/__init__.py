10683a2545815c397812c53c205d4efa86bed70f
Alexey Prudiev <alexey.prudiev@mail.ru>
2026-04-28 19:41:02 +0300

    fix: исправить упаковку workspace пакетов

---
+++ packages/contracts/src/economic_news_contracts/__init__.py
@@ -0,0 +1,10 @@
+from economic_news_contracts.analysis import AnalysisModelName, ImpactLabel
+from economic_news_contracts.events import EventEnvelope
+from economic_news_contracts.health import HealthResponse
+
+__all__ = [
+    "AnalysisModelName",
+    "EventEnvelope",
+    "HealthResponse",
+    "ImpactLabel",
+]
